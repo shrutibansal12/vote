4225cbac9681e4f0c09a3e9fa07dcc82bc6cf4b1
shrutibansal12 <66250049+shrutibansal12@users.noreply.github.com>
2020-07-18 14:55:18 +0530

    Add files via upload

---
+++ vote.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int age;
+    cout<<"\nEnter your age";
+    cin>>age;
+    
+    if(age>18)
+    { 
+    cout<<"\nYou are eligible for voting";
+    }
+    else{
+    cout<<"\nYou are not eligible for voting";
+	}
+	return 0;
+}
